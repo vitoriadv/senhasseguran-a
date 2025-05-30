0201c0fa33568868d80cc0cfcd8e0be3ba4cb63a
Vitória Degues Vieira <145033065+vitoriadv@users.noreply.github.com>
2025-05-30 07:42:06 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# senhasseguran-a
+# Gerador de senhas seguras
+Site gerador de senhas seguras desenvolvido na unidade "Segurança digital: utilizando matemática para programar senhas seguras" da Start by Alura.
+
+## Tecnologias utilizadas
+- HTML
+- CSS
+- JavaScript
+
+## Acessando o código
+Para acessar o código referente às aulas, clique no seletor `main` do repositório e selecione a aula desejada.
+
+## Notas e créditos
+Feito por @marcelopaludetto, instrutor especialista da Start by Alura.
+
+### Acesse o [Figma](https://www.figma.com/community/file/1281336077503271053/javascript-senhas-seguras-com-matematica-e-programacao) para ter acesso ao protótipo
--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 5;
+let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -24,18 +30,9 @@ function aumentaTamanho() {
     geraSenha();
 }
 
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelectorAll('.checkbox');
-
 for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
-
-
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
-const numeros = '0123456789';
-const simbolos = '!@%*?';
 
 geraSenha();
 
@@ -53,7 +50,6 @@ function geraSenha() {
     if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
     }
-    console.log(alfabeto);
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * alfabeto.length;
@@ -61,4 +57,26 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
+
 }
+
+function classificaSenha(tamanhoAlfabeto) {
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (entropia > 57) {
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35) {
+        forcaSenha.classList.add('fraca');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir essa senha.";
+}
+
+
+
+
+
